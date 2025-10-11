7080bc5888df8e53b47fd6caf821e68616ff94a0
Whopies <ianwride021@gmail.com>
2025-10-11 10:40:30 -0700

    Added the buffered window and JPanel inside JFrame. Still trying to understand stuff

--- Window.java
+++ Window.java
@@ -1,10 +1,38 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+
+import java.awt.Color;
+
+
 
 public class Window extends JFrame {
+    private int width = 800;
+    private int height = 600;
+    
     public Window() {
-        setTitle("My Window");
-        setSize(400, 300);
+        setSize(width, height);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+        getContentPane().setBackground(Color.RED);
+
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.BLUE);
+        add(panel);
+
+
+        BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int r = 3;
+                int g = 255;
+                int b = 128;
+                int rgb = (r << 16) | (g << 8) | b;
+                img.setRGB(x, y, rgb);
+            }
+        }
+
+        ImageIcon icon = new ImageIcon(img);
+        JLabel label = new JLabel(icon);
+        panel.add(label);
     }
 }
