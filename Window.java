70fa09b51186d694212f15324e9900ff1f2be012
Whopies <ianwride021@gmail.com>
2025-10-17 11:02:06 -0700

    figured out how to print a buffered image to a JPanel

--- Window.java
+++ Window.java
@@ -3,11 +3,10 @@ import java.awt.image.BufferedImage;
 
 import java.awt.Color;
 
-
-
 public class Window extends JFrame {
     private int width = 800;
     private int height = 600;
+    private static BufferedImage img;
     
     public Window() {
         setSize(width, height);
@@ -19,13 +18,12 @@ public class Window extends JFrame {
         panel.setBackground(Color.BLUE);
         add(panel);
 
-
-        BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
                 int r = 3;
                 int g = 255;
-                int b = 128;
+                int b = 0;
                 int rgb = (r << 16) | (g << 8) | b;
                 img.setRGB(x, y, rgb);
             }
@@ -34,5 +32,11 @@ public class Window extends JFrame {
         ImageIcon icon = new ImageIcon(img);
         JLabel label = new JLabel(icon);
         panel.add(label);
+
+        
     }
+    public static BufferedImage getImg() {
+            return img;
+        }
+    
 }
