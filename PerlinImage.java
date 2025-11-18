dc6da5e63f55322ec1d25da66aa7c10376bbb9af
Doot <red.doot.j@gmail.com>
2025-11-18 13:25:18 -0800

    Cleaned up code for value noise + added comments

--- PerlinImage.java
+++ PerlinImage.java
@@ -5,13 +5,15 @@ public class PerlinImage {
     private BufferedImage image;
 
     public PerlinImage(){
-        BufferedImage image = new BufferedImage(400, 400, BufferedImage.TYPE_INT_RGB);
+        BufferedImage image = new BufferedImage(750, 750, BufferedImage.TYPE_INT_RGB);
 
+        // Initialize and declare cell sizes and number of random points
         final int cellSize = 10;
         final int xCells = image.getWidth()/cellSize;
         final int yCells = image.getHeight()/cellSize;
         int[][] randPoints = new int[xCells][yCells];
 
+        // Initialize random points
         for(int y = 0; y < randPoints.length; y++){
             for(int x = 0; x < randPoints[y].length; x++){
                 
@@ -20,33 +22,37 @@ public class PerlinImage {
             }
         }
 
+        // Generate the noise image using bilinear interpolation
+        double x1, x2, y1, y2, lerp1, lerp2, lerp3;
+        int red, green, blue, rgb, X, Y;
         for (int i = 0; i < randPoints.length-1; i++)
         {
             for (int j = 0; j < randPoints[i].length-1; j++)
             {
                 // Get the corner values
-                double x1 = (double) randPoints[i][j];
-                double x2 = (double) randPoints[i][j + 1];
-                double y1 = (double) randPoints[i + 1][j];
-                double y2 = (double) randPoints[i + 1][j + 1];
-                double lerp1, lerp2, lerp3;
+                x1 = (double) randPoints[i][j];
+                x2 = (double) randPoints[i][j + 1];
+                y1 = (double) randPoints[i + 1][j];
+                y2 = (double) randPoints[i + 1][j + 1];
                 // Interpolate within the cell
-                for (double y = 0; y < 1; y+=0.01)
+                for (double y = 0; y < 1; y+=0.5/(double)cellSize)
                 {
-                    for (double x = 0; x < 1; x+=0.01)
+                    for (double x = 0; x < 1; x+=0.5/(double)cellSize)
                     {
                         // Perform bilinear interpolation
                         lerp1 = Lerp.lerp(x1, x2, x);
                         lerp2 = Lerp.lerp(y1, y2, x);
                         lerp3 = Lerp.lerp(lerp1, lerp2, y);
-                        int red = (int) lerp3;
-                        int green = (int) lerp3;
-                        int blue = (int) lerp3;
+                        red = (int) lerp3;
+                        green = (int) lerp3;
+                        blue = (int) lerp3;
 
-                        int rgb = red << 16 | green << 8 | blue;
-                        int X = (int) (x*cellSize);
-                        int Y = (int) (y*cellSize);
+                        // Combine color components into a single RGB value
+                        rgb = red << 16 | green << 8 | blue;
+
                         // Set the pixel color in the image
+                        X = (int) (x*cellSize);
+                        Y = (int) (y*cellSize);
                         image.setRGB(X + j*cellSize, Y + i*cellSize, rgb);
                     }
                 }
