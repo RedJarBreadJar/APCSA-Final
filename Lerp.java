3081b80689774c349cfd6a8c40ef34407cb85194
Doot <red.doot.j@gmail.com>
2025-11-18 10:57:23 -0800

    Implemented simple value noise in preparation for perlin noise

---
+++ Lerp.java
@@ -0,0 +1,5 @@
+public class Lerp {
+    public static double lerp(double a, double b, double t){
+        return a + t * (b - a);
+    }
+}
--- PerlinImage.java
+++ PerlinImage.java
@@ -5,41 +5,57 @@ public class PerlinImage {
     private BufferedImage image;
 
     public PerlinImage(){
-        BufferedImage image = new BufferedImage(400, 300, BufferedImage.TYPE_INT_RGB);
+        BufferedImage image = new BufferedImage(400, 400, BufferedImage.TYPE_INT_RGB);
 
-        int[][] randPoints = new int[40][30];
+        final int cellSize = 10;
+        final int xCells = image.getWidth()/cellSize;
+        final int yCells = image.getHeight()/cellSize;
+        int[][] randPoints = new int[xCells][yCells];
 
-        for (int x = 0; x < randPoints.length; x++)
-        {
-            for (int y = 0; y < randPoints[x].length; y++)
-            {
-
+        for(int y = 0; y < randPoints.length; y++){
+            for(int x = 0; x < randPoints[y].length; x++){
+                
                 int random =(int) (Math.random()*255);
-                randPoints[x][y] = random;
+                randPoints[y][x] = random;
             }
         }
 
-        for (int y = 0; y < image.getHeight(); y++)
+        for (int i = 0; i < randPoints.length-1; i++)
         {
-            for(int x = 0; x < image.getWidth(); x++)
+            for (int j = 0; j < randPoints[i].length-1; j++)
             {
-                
-                int random =(int) (Math.random()*255);
-                
-                int red = random;
-                int green = random;
-                int blue = random;
+                // Get the corner values
+                double x1 = (double) randPoints[i][j];
+                double x2 = (double) randPoints[i][j + 1];
+                double y1 = (double) randPoints[i + 1][j];
+                double y2 = (double) randPoints[i + 1][j + 1];
+                double lerp1, lerp2, lerp3;
+                // Interpolate within the cell
+                for (double y = 0; y < 1; y+=0.01)
+                {
+                    for (double x = 0; x < 1; x+=0.01)
+                    {
+                        // Perform bilinear interpolation
+                        lerp1 = Lerp.lerp(x1, x2, x);
+                        lerp2 = Lerp.lerp(y1, y2, x);
+                        lerp3 = Lerp.lerp(lerp1, lerp2, y);
+                        int red = (int) lerp3;
+                        int green = (int) lerp3;
+                        int blue = (int) lerp3;
 
-                int rgb = (red << 16) | (green << 8) | blue;
-
-                image.setRGB(x, y, rgb);
+                        int rgb = red << 16 | green << 8 | blue;
+                        int X = (int) (x*cellSize);
+                        int Y = (int) (y*cellSize);
+                        // Set the pixel color in the image
+                        image.setRGB(X + j*cellSize, Y + i*cellSize, rgb);
+                    }
+                }
             }
         }
         this.image = image;
     }
 
-    public BufferedImage getImage()
-    {
+    public BufferedImage getImage(){
         return image;
     }
 }
